6dc8ba3913e6948494200f7dc13c39f47e7573f4
Yuriy <>
2024-09-21 20:49:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Мідлвери
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://event-registration-frontend-dun.vercel.app", // Дозволити запити з цього домену
+  })
+);
 app.use(express.json());
 
 // Маршрути
